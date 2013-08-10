0ff2471785cc8673b5499f1feb0129c98dc40fe0
Filipe Magalhaes <filipe0513@gmail.com>
2013-08-10 16:26:43 -0300

    fisrt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cerveja::Application.config.secret_token = '8e848211161f6259f721b612deebf7682fd44a16315107b0913778833325ca61397ce391a36513e1cf7069a9cca1745c0d37bb46813fab3b2da99aa01ae1600d'
